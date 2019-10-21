8efbcb032f0bfd5bd963507675b6ef82ed24abb8
zakcrusted <zakcrusted@gmail.com>
2019-10-21 15:59:26 +0700

    First commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        //playerHealth = player.GetComponent <PlayerHealth> ();
+        //enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+		nav.SetDestination (player.position);
+		/*
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination (player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+        */
+    }
+}
